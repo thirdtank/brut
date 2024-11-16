bddd1294c81ac3336139cd2d8f5c37c39604fd4a
Dave Copeland <davec@naildrivin5.com>
2024-11-16 14:13:53 -0500

    change which method is used for insturmenting Sequel

--- lib/sequel/extensions/brut_instrumentation.rb
+++ lib/sequel/extensions/brut_instrumentation.rb
@@ -2,36 +2,17 @@ module Sequel
   module Extensions
     module BrutInstrumentation
       class Event < Brut::Instrumentation::Event
-        def initialize(operation:,sql:nil)
-          super(category: "sequel", name: operation, details: { sql: sql })
+        def initialize(sql:nil)
+          super(category: "sequel", name: "query", details: { sql: sql })
         end
       end
-      def execute(sql, opts = Sequel::OPTS, &block)
-        Brut.container.instrumentation.instrument(Event.new(operation: "execute", sql: sql)) do
-          super
-        end
-      end
-      def execute_dui(sql, opts = Sequel::OPTS, &block)
-        Brut.container.instrumentation.instrument(Event.new(operation: "execute_dui", sql: sql)) do
-          super
-        end
-      end
-      def execute_insert(sql, opts = Sequel::OPTS, &block)
-        Brut.container.instrumentation.instrument(Event.new(operation: "execute_insert", sql: sql)) do
-          super
-        end
-      end
-      def insert_select(*values)
-        Brut.container.instrumentation.instrument(Event.new(operation: "insert_select", sql: values)) do
-          super
-        end
-      end
-      def returning_fetch_rows(sql,&block)
-        Brut.container.instrumentation.instrument(Event.new(operation: "returning_fetch_rows", sql: sql)) do
+      def log_connection_yield(sql,conn,args=nil)
+        Brut.container.instrumentation.instrument(Event.new(sql: sql)) do
           super
         end
       end
     end
   end
   Sequel::Dataset.register_extension(:brut_instrumentation, Sequel::Extensions::BrutInstrumentation)
+  Sequel::Database.register_extension(:brut_instrumentation, Sequel::Extensions::BrutInstrumentation)
 end
