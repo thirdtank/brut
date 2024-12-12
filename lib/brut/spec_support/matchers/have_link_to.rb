a8063f32f33a6c1b03482f12d5bbcd8b0d1786a2
Dave Copeland <davec@naildrivin5.com>
2024-12-12 15:52:12 -0500

    add new matcher

---
+++ lib/brut/spec_support/matchers/have_link_to.rb
@@ -0,0 +1,14 @@
+RSpec::Matchers.define :have_link_to do |page_klass,**args|
+  match do |node|
+    node.css("a[href='#{page_klass.routing(**args)}']").any?
+  end
+
+  failure_message do |node|
+    links = node.css("a").map(&:to_html)
+    "Did not find link to #{page_klass.routing(**args)}. Found these links: #{links.join(',')}"
+  end
+
+  failure_message_when_negated do |result|
+    "Did not expect to find link to #{page_klass.routing(**args)}."
+  end
+end
