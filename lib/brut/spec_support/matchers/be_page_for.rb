ffe9125b5f8c0960ac5fd74c070c5e55ed40dfcf
Dave Copeland <davec@naildrivin5.com>
2025-01-27 14:13:48 -0500

    improve error message

--- lib/brut/spec_support/matchers/be_page_for.rb
+++ lib/brut/spec_support/matchers/be_page_for.rb
@@ -6,9 +6,10 @@ RSpec::Matchers.define :be_page_for do |klass|
   failure_message do |page|
     meta = page.locator("meta[name='class']")
     if meta.count == 0
-      "Could not find <meta name='class'> on the page:\n\n#{page.content}"
+      "Could not find <meta name='class'> on the page, which is what's needed to know what page we are on:\n\n#{page.content}"
     else
-      "Could not find <meta name='class' content='#{klass.name}'>, but found:\n\n#{meta.evaluate('e => e.outerHTML')}"
+      page_name = meta.get_attribute("content")
+      "Expected to be on page #{klass.name}, but we seem to be on page #{page_name}, based on this meta tag:\n\n#{meta.evaluate('e => e.outerHTML')}"
     end
   end
 end
