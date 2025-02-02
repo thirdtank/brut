44229f814c588856c71714c8b9fcb51100a6809c
Dave Copeland <davec@naildrivin5.com>
2025-02-02 15:35:37 -0500

    allow for skipping day of week

--- lib/brut/front_end/components/time.rb
+++ lib/brut/front_end/components/time.rb
@@ -6,7 +6,8 @@ class Brut::FrontEnd::Components::Time < Brut::FrontEnd::Component
   # @param timestamp [Time] the timestamp you wish to render. Mutually exclusive with `date`.
   # @param date [Date] the date you wish to render. Mutually exclusive with `timestamp`.
   # @param format [Symbol] the I18n format key fragment to use to locate the strftime format for formatting the timestamp.  This is appended to `"time.formats."` to form the full string. If `skip_year_if_same` is true *and* the year of this timestamp is this year, `"_no_year"` is further appended.  For example, if this value is `:full` and `skip_year_if_same` is false, the I18n key used will be `"time.formats.full"`.  If `skip_year_if_same` is true, the key would be `"time.formats.full_no_year"` only if this year is the year of the timestamp. Otherwise `"time.formats.full"` would be used.
-  # @param skip_year_if_same [true|false] if true, and this year is the same year as the timestamp, `"_no_year"` is appened to the value of `format` to form the I18n key to use.
+  # @param skip_year_if_same [true|false] if true, and this year is the same year as the timestamp, `"_no_year"` is appened to the value of `format` to form the I18n key to use.  This is applied before `skip_dow_if_not_this_week`'s suffix is.
+  # @param skip_dow_if_not_this_week [true|false] if true, and the date/timestamp is within 7 days of now, appends `"no_dow"` to the format string. If this matches a configured format, it's assumed that would be just like `format` but without the day of the week.  This is applied after `skip_year_if_same`'s suffix is.
   # @param attribute_format [Symbol] the I18n format key fragment to use to locate the strftime format for formatting *the `datetime` attribute* of the HTML element that this component renders. Generally, you want to leave this as the default of `:iso_8601`, however if you need to change it, you can.  This value is appeneded to `"time.formats."` to form the complete key. `skip_year_if_same` is not used for this value.
   # @param only_contains_class [Hash] exists because `class` is a reserved word
   # @option only_contains_class [String] :class the value to use for the `class` attribute.
@@ -15,6 +16,7 @@ class Brut::FrontEnd::Components::Time < Brut::FrontEnd::Component
     date: nil,
     format: :full,
     skip_year_if_same: true,
+    skip_dow_if_not_this_week: true,
     attribute_format: :iso_8601,
     **only_contains_class
   )
@@ -24,8 +26,24 @@ class Brut::FrontEnd::Components::Time < Brut::FrontEnd::Component
     @timestamp = timestamp || date
 
     formats = [ format ]
-    if @timestamp.year == Time.now.year && skip_year_if_same
+    use_no_year = skip_year_if_same && @timestamp.year == Time.now.year
+    use_no_dow  = if skip_dow_if_not_this_week
+                    $seven_days_ago = (Date.today - 7).to_time
+                    $timestamp      = @timestamp.to_time
+                    $timestamp < $seven_days_ago
+                  else
+                    false
+                  end
+    if use_no_year
       formats.unshift("#{format}_no_year")
+    end
+
+    if use_no_dow
+      if use_no_year
+        formats.unshift("#{format}_no_year_no_dow")
+      else
+        formats.unshift("#{format}_no_dow")
+      end
     end
 
     assumed_key_base = if @date_only
