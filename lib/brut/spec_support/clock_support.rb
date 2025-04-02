54cc0b5421c507f5b728ec101926e667590ba1d2
Dave Copeland <davec@naildrivin5.com>
2025-04-02 11:31:44 -0400

    improve clock support for tests

--- lib/brut/spec_support/clock_support.rb
+++ lib/brut/spec_support/clock_support.rb
@@ -2,10 +2,23 @@
 module Brut::SpecSupport::ClockSupport
   # Return a real lock in UTC
   def real_clock = Clock.new(TZInfo::Timezone.get("UTC"))
-  # Return a clock whose value for now is `now`
+
+  # Return a clock whose value for now is `now`, in UTC
   #
   # @param [String] now a string containing the value you want for {Clock#now} to return.
   def clock_at(now:)
-    Clock.new(TZInfo::Timezone.get("UTC"), now: Time.parse(now))
+    self.clock_in_timezone_at(timezone_name: "UTC", now: now)
+  end
+
+  # Return a clock whose value for now is `now` in the given timezone
+  #
+  # @param [String] timezone_name a string that is the name of the timezone to use.
+  # @param [String] now a string containing the value you want for {Clock#now} to return.
+  def clock_in_timezone_at(timezone_name:, now:)
+    time = Time.parse(now)
+    timezone = TZInfo::Timezone.get(timezone_name)
+    same_time_in_timezone = timezone.local_time(time.year, time.month, time.day, time.hour, time.min, time.sec)
+
+    Clock.new(TZInfo::Timezone.get(timezone_name), now: same_time_in_timezone)
   end
 end
