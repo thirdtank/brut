3a483f44292e37e368d246462f829b93e912566a
Dave Copeland <davec@naildrivin5.com>
2025-07-02 17:39:44 -0400

    document and add non-bang system

--- lib/brut/cli/executor.rb
+++ lib/brut/cli/executor.rb
@@ -16,14 +16,40 @@ class Brut::CLI::Executor
     @err = err
   end
 
-  # Execute a command, logging it to the standard output and outputing the commands output and error to the standard output and error,
-  # respecitively
+  # Execute a command, logging it to the standard output and outputing the 
+  # commands output and error to the standard output and error, respectively. If 
+  # the command exits nonzero, the exit status is returned.
+  #
+  # Generally, you want to use {#system!} for commands that must succeed
+  # for the caller to continue.  Only use this method if you need 
+  # to do special error-handling when the underlying command fails.
   #
   # @see https://docs.ruby-lang.org/en/3.3/Open3.html#method-c-popen3
+  # @see {#system!}
   #
   # @param [String|Array] args Whatever you would give to `Kernel#system` or `Open3.popen3`.
-  # @raise Brut::CLI::Error::SystemExecError if the spawed command exits nonzer
-  # @return [true]
+  # @return [int] 0 if the command completed normally, otherwise the nonzero exit status. **DO NOT TREAT THIS AS A BOOLEAN VALUE**
+  def system(*args)
+    self.system!(*args)
+    0
+  rescue Brut::CLI::SystemExecError => e
+    e.exit_status
+  end
+
+  # Execute a command, logging it to the standard output and outputing the 
+  # commands output and error to the standard output and error, respectively. If 
+  # the command exits nonzero, an exception is raised and your CLI app will
+  # also exit nonzero.
+  #
+  # If you need to handle  the command exiting nonzero, use {#system}
+  # instead, as it will not raise an exception.
+  #
+  # @see https://docs.ruby-lang.org/en/3.3/Open3.html#method-c-popen3
+  # @see {#system}
+  #
+  # @param [String|Array] args Whatever you would give to `Kernel#system` or `Open3.popen3`.
+  # @raise Brut::CLI::Error::SystemExecError if the spawed command exits nonzero
+  # @return [int] Always returns 0
   def system!(*args)
     @out.puts "Executing #{args}"
     wait_thread = Open3.popen3(*args) do |_stdin,stdout,stderr,wait_thread|
@@ -52,6 +78,6 @@ class Brut::CLI::Executor
     else
       raise Brut::CLI::SystemExecError.new(args,wait_thread.value.exitstatus)
     end
-    true
+    0
   end
 end
